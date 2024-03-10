d33bd8db5a3bb7b204f90bdbd207fccba5427e9b
ethanbowering24 <ethanbowering24@gmail.com>
2024-03-10 01:47:10 -0500

    added missing return

--- application/I2c/I2c.cpp
+++ application/I2c/I2c.cpp
@@ -9,5 +9,7 @@ namespace I2c
         status |= i2c_param_config(I2C_MASTER_NUM, &_cfg);
 
         status |= i2c_driver_install(I2C_MASTER_NUM, _cfg.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+
+        return status;
     }
 }
