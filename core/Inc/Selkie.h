769e776bf8621885b525e4b25407b3aa5c3da3ec
ethanbowering24 <ethanbowering24@gmail.com>
2024-05-11 16:00:21 -0400

    refactor done

--- core/Inc/Selkie.h
+++ core/Inc/Selkie.h
@@ -24,58 +24,53 @@
 #define LEAK_SENS_PIN static_cast<adc_channel_t>(CONFIG_LEAK_SENS_PIN)
 #define PRESSURE_SENS_ADDR static_cast<uint8_t>(CONFIG_PRESSURE_SENS_ADDR)
 
-/*class Selkie final
 
+enum class state_e //state of float
 {
-    public:*/
+    NOT_INITIALISED,
+    INITIALISED,
+    WIFI_CONNECTING,
+    WIFI_CONNECTED,
+    SOCKET_CONNECTING,
+    SOCKET_CONNECTED,
+    SENDING_DATA,
+    AWAITING_COMMAND,
+    DIVING,
+    SURFACING,
+    MISSION_COMPLETE,
+    LEAK_DETECTED,
+    ERROR
+};
 
-        enum class state_e //state of float
-        {
-            NOT_INITIALISED,
-            INITIALISED,
-            WIFI_CONNECTING,
-            WIFI_CONNECTED,
-            SOCKET_CONNECTING,
-            SOCKET_CONNECTED,
-            SENDING_DATA,
-            AWAITING_COMMAND,
-            DIVING,
-            SURFACING,
-            MISSION_COMPLETE,
-            LEAK_DETECTED,
-            ERROR
-        };
+static state_e _state{state_e::NOT_INITIALISED};
+static std::mutex state_mutx;
+static std::mutex data_mutx;
+static std::list<packet_t> data{};
 
-        static state_e _state{state_e::NOT_INITIALISED};
-        static std::mutex state_mutx;
-        static std::mutex data_mutx;
-        static std::list<packet_t> data{};
+esp_err_t setup(void);
+void loop(void);
 
-        esp_err_t setup(void);
-        void loop(void);
+Hbridge::Hbridge h1 {HBRIDGE_PIN_1, HBRIDGE_PIN_2}; 
 
-        Hbridge::Hbridge h1 {HBRIDGE_PIN_1, HBRIDGE_PIN_2}; 
+//Adc::AdcUnit adc_unit {ADC_UNIT};
 
-        //Adc::AdcUnit adc_unit {ADC_UNIT};
+//Adc::AdcChannel pressure_sens {&adc_unit, PRESSURE_SENS_PIN};
 
-        //Adc::AdcChannel pressure_sens {&adc_unit, PRESSURE_SENS_PIN};
+I2c::I2cController i2c_ctrl {};
 
-        I2c::I2cController i2c_ctrl {};
+MS5837::MS5837 psi_snsr {PRESSURE_SENS_ADDR, &i2c_ctrl};
 
-        MS5837::MS5837 psi_snsr {PRESSURE_SENS_ADDR, &i2c_ctrl};
+float pressure{0};
 
-        float pressure{0};
-        
-        float temp{0};
+float temp{0};
 
-        float depth{0};
+float depth{0};
 
-        float altitude{0};
+float altitude{0};
 
-        void record_data(void);
+void record_data(void);
 
-        WIFI::Wifi wifi;
+WIFI::Wifi wifi;
 
-        esp_err_t wifi_connect(void);
+esp_err_t wifi_connect(void);
 
-//};
