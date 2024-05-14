4eba35ae05f30ab44bbcf97822e338e56ed60f0f
Ethan Bowering <ethanbowering24@gmail.com>
2024-05-14 17:18:54 -0400

    reordered members for default initialization for cleaner code, added toString

--- core/Inc/packet.h
+++ core/Inc/packet.h
@@ -1,10 +1,18 @@
 #pragma once
 #include "ctime"
+#include <string>
+
+#define COMPANY_NUMBER static_cast<uint16_t>(CONFIG_COMPANY_NUMBER)
 
 typedef struct {
-    int companyNumber;
     time_t time;
     float pressure;
     float depth;
+    int company_number{COMPANY_NUMBER};
+
+    std::string toString()
+    {
+        return (std::to_string(company_number) + "   " + std::to_string(time) + " seconds    " + std::to_string(pressure) + " kpa    " + std::to_string(depth) + " meters\n");
+    }
 
 } packet_t;
